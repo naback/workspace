0aec17b775ba1dc3f3f434df3096098374ea15b3
Naback <naback@live.com>
2021-04-21 00:00:16 -0300

    geracao de jws implementada

---
+++ forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService
+{
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication)
+    {
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API do fórum da Alura") // quem é a aplicação que tá gerando esse token
+                .setSubject(logado.getId().toString()) // quem é o dono do token, o cara que vai receber ele
+                .setIssuedAt(hoje) // data de geração do token
+                .setExpiration(dataExpiracao)  // data de expiração do token
+                .signWith(SignatureAlgorithm.HS256, secret)  // criptografa gerando assinatura
+                .compact();
+    }
+}
--- forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,13 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,17 +19,28 @@ import javax.validation.Valid;
 @RequestMapping("/auth")
 public class AutenticacaoController
 {
+    @Autowired
+    private AuthenticationManager authManager; // Disse o instrutor que essa classe não é injetada automaticamente. Mas eu acho que isso é só na versão do Spring que ele utiliza.
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form)
     {
-        if (form != null)
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        try
         {
-            System.out.println(form.getEmail());
-            System.out.println(form.getSenha());
+            Authentication authentication = authManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+            System.out.println("Token generated: " + token);
+
+            return ResponseEntity.ok().build();
         }
-
-        // TODO: autenticar email e senha aqui
-
-        return ResponseEntity.ok().build();
+        catch (AuthenticationException e)
+        {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
