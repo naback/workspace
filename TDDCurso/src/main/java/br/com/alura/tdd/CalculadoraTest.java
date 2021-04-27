c69586357fb71df46b3a73356ed141920d5e7fdf
Naback <naback@live.com>
2021-04-26 23:07:33 -0300

    criacao de projeto de curso de tdd

---
+++ TDDCurso/src/main/java/br/com/alura/tdd/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.tdd;
+
+public class CalculadoraTest
+{
+    @Test
+    public void deveriaSomarDoisNumerosPositivos()
+    {
+        Calculadora calc = new Calculadora();
+        int soma = calc.somar(3, 7);
+
+
+    }
+
+}
