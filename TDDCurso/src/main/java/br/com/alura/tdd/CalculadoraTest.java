241c061c271105b68b1d8aa2550292711c98eec4
Naback <naback@live.com>
2021-04-26 23:20:00 -0300

    primeiro teste com junit

--- TDDCurso/src/main/java/br/com/alura/tdd/CalculadoraTest.java
+++ TDDCurso/src/main/java/br/com/alura/tdd/CalculadoraTest.java
@@ -1,5 +1,8 @@
 package br.com.alura.tdd;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculadoraTest
 {
     @Test
@@ -8,7 +11,7 @@ public class CalculadoraTest
         Calculadora calc = new Calculadora();
         int soma = calc.somar(3, 7);
 
-
+        Assert.assertEquals(10, soma);
     }
 
 }
