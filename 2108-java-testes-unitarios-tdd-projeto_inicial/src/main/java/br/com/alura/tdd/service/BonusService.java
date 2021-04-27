8a1526e5efeed1ace976f79ef7c63e18d9c47f7f
Naback <naback@live.com>
2021-04-27 19:10:52 -0300

    lancamento de excecao e correcao em codigo

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -10,9 +10,9 @@ public class BonusService
     public BigDecimal calcularBonus(Funcionario funcionario)
     {
         BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
-        if (valor.compareTo(new BigDecimal("1000")) > 0)
+        if (funcionario.getSalario().compareTo(new BigDecimal("10000")) > 0)
         {
-            valor = BigDecimal.ZERO;
+            throw new IllegalArgumentException("Funcionário com salário maior que 10 conto não pode receber bônus");
         }
         return valor.setScale(2, RoundingMode.HALF_UP);
     }
