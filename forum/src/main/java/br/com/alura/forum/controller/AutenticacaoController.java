3431fc3eee1c5d2e8f3f2a35d7b6d84f0d894e43
Naback <naback@live.com>
2021-04-28 16:11:26 -0300

    testes de controller inseridos

--- forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -20,7 +20,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
-@Profile("prod")
+@Profile(value = {"prod", "test"})
 public class AutenticacaoController
 {
     @Autowired
