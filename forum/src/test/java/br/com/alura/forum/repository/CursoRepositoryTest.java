3dfe528cc38b6bf44d8d2bbd1f28d9103312d8c1
Naback <naback@live.com>
2021-04-28 15:38:24 -0300

    insercao de anotacoes e comentarios informativos

--- forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,11 +5,15 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest  // Própria para testes de repositories -> já provê controle de transação e outros...
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) Essa configuração deve ser inserida no caso de banco de dados real e não em memória(h2)
+//@ActiveProfiles("test")  É uma boa prática separa em profile de teste para quando for testar o banco de teste... obs: vai carregar o application-test.properties ao inves do application.properties
 public class CursoRepositoryTest
 {
     @Autowired
